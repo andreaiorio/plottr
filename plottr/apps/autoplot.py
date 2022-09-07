75db23741a5aa433f84f17092c8bda0606654870
Andrea Iorio <andrea.iorio@sns.it>
2022-09-06 17:31:52 -0700

    Added derivative node

--- plottr/apps/autoplot.py
+++ plottr/apps/autoplot.py
@@ -19,6 +19,7 @@ from ..gui.widgets import MonitorIntervalInput, SnapshotWidget
 from ..node.data_selector import DataSelector
 from ..node.dim_reducer import XYSelector
 from ..node.filter.correct_offset import SubtractAverage
+from ..node.filter.derivative import Derivative
 from ..node.scaleunits import ScaleUnits
 from ..node.grid import DataGridder, GridOption
 from ..node.tools import linearFlowchart
@@ -319,6 +320,7 @@ def autoplotQcodesDataset(log: bool = False,
         ('Grid', DataGridder),
         ('Dimension assignment', XYSelector),
         ('Subtract average', SubtractAverage),
+        ('Derivative', Derivative),
         ('Scale units', ScaleUnits),
         ('plot', PlotNode)
     )
